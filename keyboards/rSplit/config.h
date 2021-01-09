dec623712d81b6d01c33ac837dbb2399212d9a6f
dajero <dajero0120@gmail.com>
2021-01-08 20:19:15 -0600

    renamed 'kb' to 'rSplit', added comments,  and removed test folder

--- keyboards/rSplit/config.h
+++ keyboards/rSplit/config.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once /* everyone else did it even though it works without it */
 
 #ifndef CONFIG_H
 #define CONFIG_H
@@ -9,24 +9,24 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
-#define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+#define MANUFACTURER    rah1236
+#define PRODUCT         rSplit 60%
+#define DESCRIPTION     tight
 
 /* key matrix size */
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 8
+#define MATRIX_ROWS 10 /* double value of actual rows */
+#define MATRIX_COLS 8 /* if uneven, set value as highest */
 
 /* key matrix pins */
 #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6, B0, B0 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6, B0, B0 } /* Have to set last 2 columns even though they don't exist */
 #define MATRIX_ROW_PINS_RIGHT { B1, F7, F6, F5, F4 }
 #define MATRIX_COL_PINS_RIGHT { C6, D7, E6, B4, B5, B6, B2, B3 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
+/* Using trrs cable between boards */
 #define USE_I2C
 
 /* Set 0 if debouncing isn't needed */
@@ -37,15 +37,18 @@
 
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
-
-/* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
 
+/* Depreciated apparently */
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
+/* Wasn't sure in documentation if this is needed for AVR but included anyways */
 #define SPLIT_USB_DETECT
 #define SPLIT_USB_TIMEOUT 2000
 #define SPLIT_USB_TIMEOUT_POLL 10
+
+/* Eeprom setup for either handedness */
+#define EE_HANDS
 
 #endif
